c043d1bd18a029615c39107a2167c069f8d544f1
akn <akane.f1991@gmail.com>
2019-10-15 17:36:35 +0900

    new commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # flask-news-feed-app
+## overview
+
+ログイン後の機能
+- RSSの追加
+- データ更新
+
+id : John
+password : Lemon
+## how to run
+```python
+pipenv shell
+pipenv install -r requirements.txt
+# set PATH+=chromedriver-path
+python run.py
+```
+
+
+
+
---
+++ flask_blog/views/entries.py
@@ -0,0 +1,61 @@
+from flask import request,redirect,url_for,render_template,flash,session
+from flask_blog import app,db,WebSite,Entry,get_feed
+from flask_sqlalchemy import Pagination
+from flask_blog.views.views import login_required
+
+#Show entries
+@app.route('/', defaults={'page': 1})
+@app.route('/entry/<int:page>')
+def index(page):
+    websites=WebSite.query.all()
+    entries = Entry.query.order_by(Entry.updated.desc()).paginate(page,per_page=10)
+    return render_template('index.html',websites=websites,entries=entries)
+
+
+#Show add website form
+@login_required
+@app.route('/website/new')
+def show_new():
+    return render_template('new.html')
+
+#Post website
+@login_required
+@app.route('/websites', methods=['POST'])
+def add_website():
+    try:
+        website = WebSite(url=request.form['url'])
+        db.session.add(website)
+        db.session.commit()
+        flash("新しくウェブサイトが追加されました")
+    except:
+        import traceback
+        traceback.print_exc()
+        flash("追加エラー")
+    finally:
+        return redirect(url_for('index'))
+
+#Get entries
+@login_required
+@app.route('/entries', methods=['POST'])
+def get_entry():
+    websites=WebSite.query.all()
+    gf_list=[get_feed(website) for website in websites]
+    w_names=[website.name for website in websites]
+    for entries,wname in zip(gf_list,w_names):
+        if entries != None:
+            for entry in entries:
+                db.session.add(entry)
+                try:
+                    db.session.commit()
+                    flash('{}の記事が１件追加されました'.format(wname))
+                except:
+                    db.session.close()
+        else:
+            flash('{}の記事は追加されませんでした'.format(wname))
+            db.session.close()
+    return redirect(url_for('index'))
+
+
+
+
+
