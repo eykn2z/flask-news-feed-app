2298523ee745826926137bf087f49136343bb80b
akn <akane.f1991@gmail.com>
2019-10-16 18:19:30 +0900

    new commit

--- flask_blog/scripts_db.py
+++ flask_blog/scripts_db.py
@@ -1,8 +1,8 @@
 from flask_script import Command
 from flask_blog import db
 
-class InitDB(Command):#Command実行のためのクラス定義
-    "create database"#クラスの説明のためのコメント
+class InitDB(Command):
+    "create database"
 
     def run(self):
         db.create_all()
